31a4730bd79fef58e11f53e218ea8886507484e8
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-03-11 08:30:29 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -16,9 +16,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'Anu-ga-ada',
+            namaowner: 'Loli_Protector',
+            instagram: 'arya_dwd',
             youtube : 'Gak Punya'
         }
     }
