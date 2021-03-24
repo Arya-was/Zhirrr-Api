35eb5cf7f692f6ae2ced3f4154c7aa416cab32ca
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-03-24 15:15:37 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -689,43 +689,6 @@ router.get('/textmaker/senja', async (req, res, next) => {
         if (theme == 'coffee-cup') {
             try {
             request.post({
-                url: "https://photooxy.com/logo-and-text-effects/put-any-text-in-to-coffee-cup-371.html",
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: `text_1=${text}&login=OK`,
-                }, (e,r,b) => {
-                    if (!e) {
-                        $ = cheerio.load(b)
-                        $(".thumbnail").find("img").each(function() {
-                            h = $(this).attr("src")
-                            var result = "https://photooxy.com/"+h
-                            fetch(encodeURI(`https://api.imgbb.com/1/upload?expiration=120&key=761ea2d5575581057a799d14e9c78e28&image=${result}&name=${randomTextNumber}`))
-                                .then(response => response.json())
-                                .then(data => {
-                                    var urlnya = data.data.url,
-                                        delete_url = data.data.delete_url;
-                                        res.json({
-                                            status : true,
-                                            creator : `${creator}`,
-                                            message : `jangan lupa follow ${creator}`,
-                                            result:{
-                                                url:urlnya,
-                                                delete_url: delete_url,
-                                                info: 'url akan hilang setelah 2 menit'
-                                            }
-                                        })
-                                })
-                        })
-                    }
-                })
-                } catch (e) {
-                	console.log(e);
-                res.json(loghandler.error)
-                }
-		if (theme == 'star') {
-            try {
-            request.post({
                 url: "https://photooxy.com/logo-and-text-effects/write-stars-text-on-the-night-sky-200.html",
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
