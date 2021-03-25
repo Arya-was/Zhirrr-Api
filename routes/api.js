2d6499bac7f2f468ad3f7fcb0c21487203bfce69
Loli Protector <74448616+Arya-was@users.noreply.github.com>
2021-03-25 11:45:55 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -683,9 +683,46 @@ router.get('/textmaker/senja', async (req, res, next) => {
 	if(!apikeyInput) return res.json(loghandler.notparam)
 	if(apikeyInput != 'anu') return res.json(loghandler.invalidKey)
         if (!theme) return res.json(loghandler.nottheme)
-        if (theme != 'coffee-cup' && theme != 'coffee-cup2') return res.json(loghandler.notheme)
+        if (theme != 'coffee-cup' && theme != 'coffee-cup2' && theme != 'gradient') return res.json(loghandler.notheme)
         if (!text) return res.json(loghandler.nottext)
 
+	if (theme == 'gradient') {
+            try {
+            request.post({
+                url: "https://photooxy.com/logo-and-text-effects/gradient-avatar-text-effect-207.html",
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: `text_1=${text}&login=OK`,
+                }, (e,r,b) => {
+                    if (!e) {
+                        $ = cheerio.load(b)
+                        $(".thumbnail").find("img").each(function() {
+                            h = $(this).attr("src")
+                            var result = "https://photooxy.com/"+h
+                            fetch(encodeURI(`https://api.imgbb.com/1/upload?expiration=120&key=761ea2d5575581057a799d14e9c78e28&image=${result}&name=${randomTextNumber}`))
+                                .then(response => response.json())
+                                .then(data => {
+                                    var urlnya = data.data.url,
+                                        delete_url = data.data.delete_url;
+                                        res.json({
+                                            status : true,
+                                            creator : `${creator}`,
+                                            message : `jangan lupa follow ${creator}`,
+                                            result:{
+                                                url:urlnya,
+                                                delete_url: delete_url,
+                                                info: 'url akan hilang setelah 2 menit'
+                                            }
+                                        })
+                                })
+                        })
+                    }
+                })
+                } catch (e) {
+                	console.log(e);
+                res.json(loghandler.error)
+                }
         if (theme == 'coffee-cup') {
             try {
             request.post({
